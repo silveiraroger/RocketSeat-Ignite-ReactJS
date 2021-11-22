4e9cdde32b9ed54570020ea8bef576a216716601
roger.silveira <rogersilveira_13@hotmail.com>
2021-11-22 09:01:56 -0300

    Criação da página e integração com o Stripe

--- Projetos/03-ignews/src/pages/index.tsx
+++ Projetos/03-ignews/src/pages/index.tsx
@@ -1,12 +1,63 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
+import { stripe } from '../services/stripe'
+import { SubscribeButton } from '../componentes/SubscribeButton'
 
-export default function Home() {
+import styles from './home.module.scss'
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
-        <title>Inicio - ig.news</title>
+        <title>Home - ig.news</title>
       </Head>
-      <h1>Hello</h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all publications <br />
+            <span>for {product.amount} month</span>
+          </p>
+
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
+}
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number
+  }
+}
+
+//Faz a requisição toda vez que é chamado
+//export const getServerSideProps: GetServerSideProps = async () => {
+
+//
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1JyahBJqzDx6nAVIqJKk7WZ0', {
+    expand: ['product']
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24 //24 horas (tempo em segundos)
+  }
 }
